8aff636d564da95827d0e900ce2a2650985a088a
parthiv <118996592+karthikeyaparthiv@users.noreply.github.com>
2024-04-12 17:43:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ async function run() {
 run().catch(console.dir);
 
 mongoose.connect(
-  process.env.MONGODB_CONNECT_URI
+  process.env.MONGODB_CONNECT_URI ,
    // "mongodb://localhost:27017/ecom",
    { bufferTimeoutMS: 40000 }
     );
